f331a2d3f27b4b44a33fa065783b7884cc2689a8
Robert Lohr <4004615+the-codeslinger@users.noreply.github.com>
2021-01-06 15:35:19 +0100

    Fix test-main to use QCoreApplication

--- wt2-shared-test/main.cpp
+++ wt2-shared-test/main.cpp
@@ -1,7 +1,16 @@
+#include <QTest>
+
 #include "testrunner.h"
 
 int
 main(int argc, char* argv[])
 {
+
+
+    QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+
+    QTEST_SET_MAIN_SOURCE_PATH;
+
     return Test::TestRunner::run(argc, argv);
 }
